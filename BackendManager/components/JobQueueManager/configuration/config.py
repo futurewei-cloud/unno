63aeec76f2ac4bc59219af1e8546c15c634478c8
Lin Chen <gggchenlin@gmail.com>
2019-11-27 22:32:05 -0800

    update tracking module to the correct port

--- BackendManager/components/JobQueueManager/configuration/config.py
+++ BackendManager/components/JobQueueManager/configuration/config.py
@@ -7,7 +7,7 @@ class SQLConfig:
 
 class ServiceConfig:
     # tracking module
-    backend_endpoint = 'http://10.145.83.34:5011'
+    backend_endpoint = 'http://10.145.83.34:5012'
     result_api = backend_endpoint + '/api/v1/annotation'
 
 
